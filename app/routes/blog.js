7889225e6b6eabb738fed2a84ce42c0f096776b9
Gloria Friesen and Ryan McAlpin <gloria@>
2017-04-26 15:16:26 -0700

    add delete

--- app/routes/blog.js
+++ app/routes/blog.js
@@ -3,5 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('blog', params.blog_id);
+  },
+  actions: {
+    delete(model) {
+      if(confirm('Are you sure?')) {
+        model.destroyRecord();
+        this.transitionTo('index');
+      }
+    }
   }
 });
